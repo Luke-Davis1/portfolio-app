5ce045c8b830ab5a2a25fcb3c2d25b98cef9158e
Luke-Davis1 <luke.e.davis2@gmail.com>
2024-07-20 20:27:42 -0600

    Updating experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -5,36 +5,42 @@ const Experience = () => {
 			<h3>Experience</h3>
 			<div className={classes.container}>
 				<div className={classes["experience-item"]}>
-					<h4>WEB DEVELOPER (2 years)</h4>
+					<h4>TECHNICAL INTERN</h4>
 					<ul>
-						<li>Developed two websites using Wix Content Management system</li>
 						<li>
-							Implemented third party libraries and APIs to populate tables and
-							calendars
+							Spearheaded implementation of Playwright's parallel processing and
+							orchestrated Azure DevOps multi-stage setup, achieving a 75%
+							reduction in front-end automation test runtime.
 						</li>
 						<li>
-							Created Custom elements to allow music to be played on website
-							from radio station
+							Developed and maintained 15+ frontend automation scripts using
+							Playwright and JavaScript, covering critical user scenarios such
+							as data entry, search functionalities, and deletion operations.
 						</li>
 						<li>
-							Worked on both front-end user interface and back-end database
+							Implemented 10+ backend automation scripts in Java to validate 40+
+							API endpoints, ensuring data integrity and seamless communication
+							between frontend and backend systems.
 						</li>
 					</ul>
 				</div>
 				<div className={classes["experience-item"]}>
-					<h4>STUDENT (3 years)</h4>
+					<h4>WEB/APPLICATION DEVELOPER</h4>
 					<ul>
 						<li>
-							Obtained training and practice in multiple programming languages
-						</li>
-						<li>Maintained a 4.0 GPA</li>
-						<li>
-							Double majoring in Computer Science and Computational Statistics
-							and Data Science
+							Developed JavaScript modules to integrate and parse data from
+							multiple third-party APIs, significantly improving data retrieval
+							times for real-time updates and dynamic content display.
 						</li>
 						<li>
-							Mentored by faculty in both the Math and Computer Science
-							Department
+							Collaborated with professors specializing in web development to
+							implement SEO strategies, resulting in a 40% increase in organic
+							search traffic.
+						</li>
+						<li>
+							Utilized programming fundamentals to troubleshoot and resolve
+							technical issues, ensuring optimal performance and usability of
+							developed websites.
 						</li>
 					</ul>
 				</div>
